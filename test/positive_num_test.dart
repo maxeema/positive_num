1c7ec821b4907856fd8535350bbcd8b72259ac8c
Maxeem <jmax.posh@gmail.com>
2023-10-07 21:47:53 -0300

    add a test case for num, int, and double

--- test/positive_num_test.dart
+++ test/positive_num_test.dart
@@ -5,6 +5,18 @@ import 'package:test/test.dart';
 
 void main() {
   group('PositiveNum', () {
+    test(
+        'Ensure that PositiveNum can be created with a num, int, or double reference',
+        () {
+      const num numValue = 1;
+      expect(PositiveNum.create(numValue).instance, isNotNull);
+
+      const int intValue = 2;
+      expect(PositiveNum.create(intValue).instance, isNotNull);
+
+      const double doubleValue = 3.3;
+      expect(PositiveNum.create(doubleValue).instance, isNotNull);
+    });
     test('Ensure that a PositiveNum is created with a positive number', () {
       const nums1 = <num>[0.001, 1, 0.1, 0.000001, 2, 1, 99999];
       const nums2 = <num>[
@@ -33,18 +45,18 @@ void main() {
       expect(PositiveNum.create(1).instance!.value, isA<int>());
       expect(PositiveNum.create(2.2).instance!.value, isA<double>());
     });
-    test('Ensure that "==" is overriden correctly', () {
+    test('Ensure that "==" is overridden correctly', () {
       final num1 = PositiveNum.create(1).instance!;
       expect(num1, equals(num1));
       expect(num1, equals(PositiveNum.create(1).instance));
       expect(num1, same(num1));
       expect(num1, isNot(same(PositiveNum.create(1).instance)));
     });
-    test('Ensure that "toString()" is overriden correctly', () {
+    test('Ensure that "toString()" is overridden correctly', () {
       expect(PositiveNum.create(1).instance.toString(), '1');
       expect(PositiveNum.create(2.2).instance.toString(), '2.2');
     });
-    test('Ensure that "hashCode" is overriden correctly', () {
+    test('Ensure that "hashCode" is overridden correctly', () {
       expect(PositiveNum.create(1).instance.hashCode, 1.hashCode);
       expect(PositiveNum.create(2.2).instance.hashCode, 2.2.hashCode);
     });
