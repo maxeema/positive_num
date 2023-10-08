9956f6c4d4809316d57262ac2dc3e4de2a4e8a9f
Maxeem <jmax.posh@gmail.com>
2023-10-07 21:38:42 -0300

    refactor tests

--- test/positive_num_test.dart
+++ test/positive_num_test.dart
@@ -5,48 +5,48 @@ import 'package:test/test.dart';
 
 void main() {
   group('PositiveNum', () {
-    test('Ensure that a PositiveNum is created with a positive num', () {
-      final nums1 = [0.001, 1, 0.1, 0.000001, 2, 1, 99999];
-      final nums2 = [double.minPositive, double.maxFinite, double.infinity];
-      for (var num in [...nums1, ...nums2]) {
-        final (:error, :instance) = PositiveNum.create(num);
-        expect(error, isNull);
+    test('Ensure that a PositiveNum is created with a positive number', () {
+      const nums1 = <num>[0.001, 1, 0.1, 0.000001, 2, 1, 99999];
+      const nums2 = <num>[
+        double.minPositive,
+        double.maxFinite,
+        double.infinity
+      ];
+      for (final number in const [...nums1, ...nums2]) {
+        final (:error, :instance) = PositiveNum.create(number);
+        expect(error, null);
         expect(instance!.value, greaterThan(0));
-        expect(instance.value, equals(num));
+        expect(instance.value, number);
       }
     });
-    test('Ensure that a PositiveNum is not created with a non-positive num',
+    test('Ensure that a PositiveNum is not created with a non-positive number',
         () {
-      final nums1 = [-5, -5.0, -0.001, -0.0, 0.0, 0];
-      final nums2 = [double.nan, double.negativeInfinity];
-      for (var num in [...nums1, ...nums2]) {
-        final (:error, :instance) = PositiveNum.create(num);
+      const nums1 = <num>[-5, -5.0, -0.001, -0.0, 0.0, 0];
+      const nums2 = <num>[double.nan, double.negativeInfinity];
+      for (final number in const [...nums1, ...nums2]) {
+        final (:error, :instance) = PositiveNum.create(number);
         expect(error, isNotNull);
-        expect(instance, isNull);
+        expect(instance, null);
       }
     });
     test('Ensure that generic T has a proper type either int or double', () {
-      // ensure that the value has type of 'int'
-      int intValue = PositiveNum.create(1).instance!.value;
-
-      // ensure that the value has type of 'double'
-      double doubleValue = PositiveNum.create(1.0).instance!.value;
+      expect(PositiveNum.create(1).instance!.value, isA<int>());
+      expect(PositiveNum.create(2.2).instance!.value, isA<double>());
     });
     test('Ensure that "==" is overriden correctly', () {
       final num1 = PositiveNum.create(1).instance!;
       expect(num1, equals(num1));
-      expect(num1, equals(PositiveNum.create(1).instance!));
-
+      expect(num1, equals(PositiveNum.create(1).instance));
       expect(num1, same(num1));
-      expect(num1, isNot(same(PositiveNum.create(1).instance!)));
+      expect(num1, isNot(same(PositiveNum.create(1).instance)));
     });
     test('Ensure that "toString()" is overriden correctly', () {
-      final num1 = PositiveNum.create(1).instance!;
-      expect(num1.toString(), equals('1'));
+      expect(PositiveNum.create(1).instance.toString(), '1');
+      expect(PositiveNum.create(2.2).instance.toString(), '2.2');
     });
     test('Ensure that "hashCode" is overriden correctly', () {
-      final num1 = PositiveNum.create(1).instance!;
-      expect(num1.hashCode, equals(1.hashCode));
+      expect(PositiveNum.create(1).instance.hashCode, 1.hashCode);
+      expect(PositiveNum.create(2.2).instance.hashCode, 2.2.hashCode);
     });
   });
 }
